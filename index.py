e75042c73bcd921e9b06cfd6bf9c7ceb62467c71
ayyappakotla <ayyappa23051995@gmail.com>
2024-07-29 13:57:33 +0530

    Updated the lambda logic to store the file in s3

--- index.py
+++ index.py
@@ -1,25 +1,33 @@
 import boto3
 import logging
+import tempfile
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 ssm_param_name = '/sample/lambda-extract'
+s3_bucket_name = 'dcx-sample-bucket'
+s3_file_name = 'ssm_params.txt'
 
 
 def lambda_handler(event, context):
     try:
-        ssm = boto3.client('ssm')
-        response = ssm.get_parameter(Name=ssm_param_name)
+        logger.info("Connecting to SSM")
+        ssm_client = boto3.client('ssm')
+        s3_client = boto3.client('s3')
+
+        response = ssm_client.get_parameter(Name=ssm_param_name)
         logger.info(f"response: {response}")
 
         parameter_value = response['Parameter']['Value']
 
-        response_data = {
-            'String': parameter_value
-        }
+        temp_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
+        temp_file.write(parameter_value)
+        temp_file.close()
 
-        return response_data
+        s3_client.upload_file(temp_file.name, s3_bucket_name, s3_file_name)
+
+        return parameter_value
     except Exception as e:
         raise e
 
